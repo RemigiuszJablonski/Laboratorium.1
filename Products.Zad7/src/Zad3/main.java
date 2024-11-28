404bfaaa43a970147b29541ddb42e344415bc685
RemigiuszJablonski <157613902+RemigiuszJablonski@users.noreply.github.com>
2024-11-28 13:30:53 +0100

    Add files via upload

---
+++ Products.Zad7/src/Zad3/main.java
@@ -0,0 +1,31 @@
+package Zad3;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        List<products> product = new ArrayList<>();
+        product.add(new products("Cola", 4.50F));
+        product.add(new products("Potatoes", 2.00F));
+        product.add(new products("Corn", 1.00F));
+        product.add(new products("Pizza", 30.99F));
+        product.add(new products("Almonds", 15.99F));
+
+        System.out.println("Sorted products by name by Comparable\n");
+        Collections.sort(product);
+        for(products productss : product){
+            System.out.println(productss);
+        }
+
+        System.out.println("\nSorted products by cost by Comparator\n");
+        product.sort(new Comparator<products>() {
+            @Override
+            public int compare(products a, products b) {
+                return Double.compare(a.getCost(), b.getCost()); // Porównanie cen
+            }
+        });
+        for (products productss : product) {
+            System.out.println(productss);
+        }
+        }
+    }
+
